4ac83c9051f07ddc626801120e6bf85be358be15
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-01-09 20:02:22 -0600

    looks the same, clear all button works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import ToDoList from './components/TodoList';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      tasks: 'dhsjfhjdsfhjk'
+    }; 
+  }
+
+  handleChangeFunction = (event) => {
+    this.setState({tasks: event.target.value});
+  };
+
+  handleTwo = event => {
+    this.setState({tasks: ''});
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List: MVP</h1>
+        <input onChange ={this.handleChangeFunction} />
+        <p>{this.state.tasks}</p>
+        <button>Add To Do</button> 
+        <button onClick = {this.handleTwo} >Clear Completed</button>
       </div>
     );
   }
